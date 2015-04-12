b144ec6debba0b922d26cb81c9711300ca6aef7c
lispc <mycinbrin@gmail.com>
2015-04-13 00:24:51 +0800

    to push

--- java/test_join.sh
+++ java/test_join.sh
@@ -1,7 +1,7 @@
 set -e
 javac -cp lib/commons-io-2.4.jar -sourcepath src -d build/classes src/edu/tsinghua/dbgroup/*
-#time java -cp lib/commons-io-2.4.jar:./build/classes edu.tsinghua.dbgroup.EditDistanceJoinerTest 2 performance/author.data > tmp.out
-time java -Xprof -cp lib/commons-io-2.4.jar:./build/classes edu.tsinghua.dbgroup.EditDistanceJoinerTest 2 performance/author.data > tmp.out
+time java -cp lib/commons-io-2.4.jar:./build/classes edu.tsinghua.dbgroup.EditDistanceJoinerTest 2 performance/author.data > tmp.out
+#time java -Xprof -cp lib/commons-io-2.4.jar:./build/classes edu.tsinghua.dbgroup.EditDistanceJoinerTest 2 performance/author.data > tmp.out
 if diff tmp.out performance/trim.txt
 then
     echo 'test succ'
