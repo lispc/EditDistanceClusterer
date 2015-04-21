ddaacaa8fcd6250a7206ea9272679b0cf1460e24
lispc <mycinbrin@gmail.com>
2015-04-21 23:30:31 +0800

    rename test data dir name

--- test_cluster.sh
+++ test_cluster.sh
@@ -2,8 +2,8 @@ set -e
 ant
 mkdir -p build/sample_classes
 javac -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar -sourcepath sample -d build/sample_classes sample/edu/tsinghua/dbgroup/sample/EditDistanceClustererTest.java
-java -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar:./build/sample_classes edu.tsinghua.dbgroup.sample.EditDistanceClustererTest 1 data/cluster.small.in > data/cluster.small.out
-if diff data/cluster.small.out data/cluster.small.std
+java -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar:./build/sample_classes edu.tsinghua.dbgroup.sample.EditDistanceClustererTest 1 testdata/cluster.small.in > testdata/cluster.small.out
+if diff testdata/cluster.small.out testdata/cluster.small.std
 then
     echo 'EditDistanceClusterer Test Succeed'
 fi
--- test_join.sh
+++ test_join.sh
@@ -2,8 +2,8 @@ set -e
 ant
 mkdir -p build/sample_classes
 javac -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar -sourcepath sample -d build/sample_classes sample/edu/tsinghua/dbgroup/sample/EditDistanceJoinerTest.java
-time java -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar:./build/sample_classes edu.tsinghua.dbgroup.sample.EditDistanceJoinerTest 2 data/join.big.in > data/join.big.out
-if diff data/join.big.out data/join.big.std
+time java -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar:./build/sample_classes edu.tsinghua.dbgroup.sample.EditDistanceJoinerTest 2 testdata/join.big.in > testdata/join.big.out
+if diff testdata/join.big.out testdata/join.big.std
 then
     echo 'EditDistanceJoiner Test Succeed'
 fi
