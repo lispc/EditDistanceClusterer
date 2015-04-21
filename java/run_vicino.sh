7847b1c493efc9891d4d6c657d127b9e9c298cb5
lispc <mycinbrin@gmail.com>
2015-04-21 20:33:32 +0800

    refine dir

--- java/run_vicino.sh
+++
@@ -1,4 +0,0 @@
-ant
-mkdir -p build/sample_classes
-javac -cp lib/commons-io-2.4.jar:lib/vicino-zz.jar:./build/jar/EditDistanceJoiner.jar -sourcepath sample -d build/sample_classes sample/edu/tsinghua/dbgroup/sample/VicinoTester.java
-time java -cp lib/commons-io-2.4.jar:lib/vicino-zz.jar:lib/secondstring-20100303.jar:./build/sample_classes edu.tsinghua.dbgroup.sample.VicinoTester 2 performance/author.data > vicino_out_zz
--- test_join.sh
+++ test_join.sh
@@ -1,10 +1,9 @@
 set -e
 ant
 mkdir -p build/sample_classes
-javac -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar -sourcepath sample -d build/sample_classes sample/edu/tsinghua/dbgroup/sample/*
-time java -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar:./build/sample_classes edu.tsinghua.dbgroup.sample.EditDistanceJoinerTest 2 performance/author.data > tmp.out
-#time java -Xprof -cp lib/commons-io-2.4.jar:./build/classes edu.tsinghua.dbgroup.EditDistanceJoinerTest 2 performance/author.data > tmp.out
-if diff tmp.out performance/trim.txt
+javac -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar -sourcepath sample -d build/sample_classes sample/edu/tsinghua/dbgroup/sample/EditDistanceJoinerTest.java
+time java -cp lib/commons-io-2.4.jar:./build/jar/EditDistanceJoiner.jar:./build/sample_classes edu.tsinghua.dbgroup.sample.EditDistanceJoinerTest 2 data/join.big.in > data/join.big.out
+if diff data/join.big.out data/join.big.std
 then
-    echo 'test succ'
+    echo 'EditDistanceJoiner Test Succeed'
 fi
